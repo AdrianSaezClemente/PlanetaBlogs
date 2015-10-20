ff88abcd89ecf93b3afc9fb56274e0fcabaaefee
Adri <adry21ciru@gmail.com>
2015-10-20 18:08:44 +0200

    Botones agregar y eliminar hilo en presentación de profesor.

--- TFG/static/presentacionprof.js
+++ TFG/static/presentacionprof.js
@@ -24,7 +24,7 @@ function Agregar(idasignatura,titulo,descripcion){
 		},
 	});
 	$("#nohayent").hide()
-	$("#clase_lista_asignaturas").prepend("<li id='eliminar"+idasignatura+"' class='list-group-item nuevo'><div style='word-wrap:break-word;'><h6 class='list-group-item-heading'><div class='row'><div class='col-sm-9'><span><strong>Título: </strong>"+titulo+"</span></br><span><strong>Descripción: </strong>"+descripcion+"</span></div><div class='col-sm-3'><button type='button' class='btn-xs btn-warning pull-right boton' onclick=window.open('/planetablogs/tutores/hilo/"+idasignatura+"','_self')><span class='glyphicon glyphicon-camera'></span> Visitar</button><button onclick=EliminarHilo('"+idasignatura+"','"+titulo+"','"+descripcion+"'); id='botoneliminar"+idasignatura+"' title='Eliminar hilo' type='button' class='btn-xs btn-danger pull-right boton'><span class='glyphicon glyphicon-remove'></span> Eliminar</button></div></div></h6></div></li></br>");
+	$("#clase_lista_asignaturas").prepend("<li id='eliminar"+idasignatura+"' class='list-group-item nuevo'><div style='word-wrap:break-word;'><h6 class='list-group-item-heading'><div class='row'><div class='col-sm-9'><span><strong>Título: </strong>"+titulo+"</span></br><span><strong>Descripción: </strong>"+descripcion+"</span></div><div class='col-sm-3'><button type='button' class='btn-xs btn-warning pull-right boton' onclick=window.open('/planetablogs/tutores/hilo/"+idasignatura+"','_self')><span class='glyphicon glyphicon-camera'></span> Visitar</button><button onclick='EliminarHilo(`"+idasignatura+"`,`"+titulo+"`,`"+descripcion+"`);' id='botoneliminar"+idasignatura+"' title='Eliminar hilo' type='button' class='btn-xs btn-danger pull-right boton'><span class='glyphicon glyphicon-remove'></span> Eliminar</button></div></div></h6></div></li></br>");
 	$("li#"+idasignatura).hide("highlight", 2000 );
 }
 
@@ -68,7 +68,7 @@ function Eliminar(idasignatura,titulo,descripcion){
 	});
 	var html = "<div class='list-group'><li id="+idasignatura+" class='list-group-item'><div style='word-wrap:break-word;'>";
 	html += "<h6  class='list-group-item-heading'><div class='row'><div class='col-sm-9'><span><strong>Título: </strong>"+titulo+"</span></br><span><strong>Descripción: </strong>"+descripcion+"</span>";
-	html += "</div><div id='botonagregar"+idasignatura+"' class='col-sm-3'><button onclick=Agregar('"+idasignatura+"','"+titulo+"','"+descripcion+"'); id='agregar"+idasignatura+"' title='Añadir hilo' type='button' class='btn-xs btn-primary pull-right boton'><span class='glyphicon glyphicon-plus'></span>Añadir</button>";
+	html += "</div><div id=botonagregar"+idasignatura+" class='col-sm-3'><button onclick='Agregar(`"+idasignatura+"`,`"+titulo+"`,`"+descripcion+"`);' id=agregar"+idasignatura+" title='Añadir hilo' type='button' class='btn-xs btn-primary pull-right boton'><span class='glyphicon glyphicon-plus'></span>Añadir</button>";
 	html += "</div></div></h6></div></li></div>";
 	$("#clase_no_lista_asignaturas").prepend(html);
 }
